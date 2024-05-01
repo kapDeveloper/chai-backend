20a13c30cabab25d0b023fc88254ca4d5d7790cd
ts <kap@globalia.com>
2024-05-01 13:01:52 +0530

    database connect successfully

---
+++ src/index.js
@@ -0,0 +1,35 @@
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+
+// require("dotenv").config({ path: "../.env" });
+
+import dotenv from "dotenv";
+import connectDB from "./db/db.js";
+
+dotenv.config({
+  path: "../.env",
+});
+
+connectDB();
+
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    app.on("Error", (error) => {
+      console.log("Error: ", error);
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("Error: ", error);
+    throw error;
+  }
+})();
+
+*/
